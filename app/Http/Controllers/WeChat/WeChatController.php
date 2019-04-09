6b303026361bdc4113fb9b77f0bac9efd221fd03
yuanshuai <yuanshuai@localhost.localdomain>
2019-04-09 07:54:04 +0000

    xml

--- app/Http/Controllers/WeChat/WeChatController.php
+++ app/Http/Controllers/WeChat/WeChatController.php
@@ -18,7 +18,7 @@ class WeChatController extends Controller
     public function WXEvent(){
         $data = file_get_contents("php://input");//通过流的方式接受post数据
         $xml = simplexml_load_string($data);//把xml转化成对象
-        $time = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
+        $time = date('Y-m-d H:i:s') . "\n" . $xml . "\n<<<<<<<";
         file_put_contents("logs/wx_event.log",$time,FILE_APPEND);
         echo "SUCCESS";
    }
