c863ad26cf7b3d5cc6c198189dec4cbe386abdab
yuanshuai <yuanshuai@localhost.localdomain>
2019-04-10 14:25:16 +0000

    自定义菜单测试

--- app/Http/Controllers/WeChat/WeChatController.php
+++ app/Http/Controllers/WeChat/WeChatController.php
@@ -27,9 +27,9 @@ class WeChatController extends Controller
         $Event=$obj->Event;
         if($MsgType=="event"){
             if($Event=="subscribe"){
-                $userInfo=$this->userInfo($FromUserName);
+                $userInfo=$this->userInfo($FromUserName);//获取用户昵称
 
-                $one=UserModel::where(['openid'=>$FromUserName])->first();
+                $one=UserModel::where(['openid'=>$FromUserName])->first();//查询数据库
                 if($one){
                     $xml="<xml>
                               <ToUserName><![CDATA[$FromUserName]]></ToUserName>
@@ -94,5 +94,81 @@ class WeChatController extends Controller
         $count=file_get_contents($url);
         $u=json_decode($count,true);
         return $u;
+    }
+
+    public function customize(){
+
+        $access=$this->getAccessToken();
+        $url=" https://api.weixin.qq.com/cgi-bin/menu/create?access_token=$access";
+        $data = '{
+            "button":[
+     {
+         "type":"click",
+          "name":"今日歌曲",
+          "key":"V1001_TODAY_MUSIC"
+      },
+      {
+          "name":"菜单",
+           "sub_button":[
+           {
+               "type":"view",
+               "name":"搜索",
+               "url":"http://www.soso.com/"
+            },
+            {
+                "type":"click",
+               "name":"赞一下我们",
+               "key":"V1001_GOOD"
+            }]
+       }]
+ }';
+
+//        $data = json_decode($data,true);
+        $json = $this->curlRequest($url,$data);
+        echo $json;
+    }
+
+
+    public function getPost($url,$vars){
+
+        $ch = curl_init();
+        $params[CURLOPT_URL] = $url;    //请求url地址
+        $params[CURLOPT_HEADER] = false; //是否返回响应头信息
+        $params[CURLOPT_RETURNTRANSFER] = true; //是否将结果返回
+        $params[CURLOPT_FOLLOWLOCATION] = true; //是否重定向
+        $params[CURLOPT_USERAGENT] = 'Mozilla/5.0 (Windows NT 5.1; rv:9.0.1) Gecko/20100101 Firefox/9.0.1';
+
+        $postfields = '';
+        foreach ($vars as $key => $value){
+            $postfields .= urlencode($key) . '=' . urlencode($value) . '&';
+        }
+        $params[CURLOPT_POST] = true;
+        $params[CURLOPT_POSTFIELDS] = $postfields;
+
+        //解决方案一 禁用证书验证
+        $params[CURLOPT_SSL_VERIFYPEER] = false;
+        $params[CURLOPT_SSL_VERIFYHOST] = false;
+
+        curl_setopt_array($ch, $params); //传入curl参数
+        return  curl_exec($ch); //执行
+    }
+
+    public function curlRequest($url,$data = ''){
+        $ch = curl_init();
+        $params[CURLOPT_URL] = $url;    //请求url地址
+        $params[CURLOPT_HEADER] = false; //是否返回响应头信息
+        $params[CURLOPT_RETURNTRANSFER] = true; //是否将结果返回
+        $params[CURLOPT_FOLLOWLOCATION] = true; //是否重定向
+        $params[CURLOPT_TIMEOUT] = 30; //超时时间
+        if(!empty($data)){
+            $params[CURLOPT_POST] = true;
+            $params[CURLOPT_POSTFIELDS] = $data;
+        }
+        $params[CURLOPT_SSL_VERIFYPEER] = false;//请求https时设置,还有其他解决方案
+        $params[CURLOPT_SSL_VERIFYHOST] = false;//请求https时,其他方案查看其他博文
+        curl_setopt_array($ch, $params); //传入curl参数
+        $content = curl_exec($ch); //执行
+        curl_close($ch); //关闭连接
+        return $content;
     }
 }
